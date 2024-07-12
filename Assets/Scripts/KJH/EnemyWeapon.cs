2b1696ec2d1ede6de5222ec5384a00a0de6f6177
changheon <amckdgjs1@gmail.com>
2024-07-12 11:46:16 +0900

    [Update]회전하는 적, EnemyWeaponType 구현

--- Assets/Scripts/KJH/EnemyWeapon.cs
+++ Assets/Scripts/KJH/EnemyWeapon.cs
@@ -22,65 +22,71 @@ public class EnemyWeapon : MonoBehaviour
     [Range(0, 0.99f)][SerializeField] float projectionSpeed_RandomGain;
 
     public void CommandFire(Vector3 targetPosition)
-    {
-        GameObject obj = ObjectPoolManager.Instance.DequeueObject(Prefab_Projectile);
-
+    {        
         switch (projectionType)
         {
             case ProjectionType.Common:
-                Fire_Common(obj, targetPosition);
+                Fire_Common(targetPosition);
                 break;
             case ProjectionType.Shogun:
-                Fire_ShotGun(obj, targetPosition);
+                Fire_ShotGun(targetPosition);
                 break;
             case ProjectionType.Scatter:
-                Fire_Scatter(obj, targetPosition);
+                Fire_Scatter(targetPosition);
                 break;
             case ProjectionType.Sniping:
-                Fire_Sniping(obj, targetPosition);
+                Fire_Sniping(targetPosition);
                 break;
             default:
-                Fire_Common(obj, targetPosition);
+                Fire_Common(targetPosition);
                 break;
         }
     }
 
-    void Fire_Common(GameObject obj, Vector3 target)
+    void Fire_Common(Vector3 target)
     {
-        Vector3 projectionVector = (target - this.transform.position) * speed_Projectile;
+        GameObject obj = ObjectPoolManager.Instance.DequeueObject(Prefab_Projectile);
+
+        Vector3 projectionVector = (target - this.transform.position).normalized * speed_Projectile;
         obj.GetComponent<Bullet>().Shoot(this.transform.position, projectionVector);
     }
-    void Fire_ShotGun(GameObject obj, Vector3 target)
+    void Fire_ShotGun(Vector3 target)
     {
         for (int i = 0; i < projection_ea; i++)
         {
+            GameObject obj = ObjectPoolManager.Instance.DequeueObject(Prefab_Projectile);
+
             float randomProjectionAngle = Random.Range(-projection_Angle * 0.5f, projection_Angle * 0.5f);
             float randomProjecitonVelocity = speed_Projectile * (1 + Random.Range(-projectionSpeed_RandomGain, projectionSpeed_RandomGain));
             Quaternion angle = Quaternion.Euler(0, randomProjectionAngle, 0);
 
-            Vector3 projectionVector = (target - this.transform.position) * randomProjecitonVelocity;
+            Vector3 projectionVector = (target - this.transform.position).normalized * randomProjecitonVelocity;
             projectionVector = angle * projectionVector;
 
             obj.GetComponent<Bullet>().Shoot(this.transform.position, projectionVector);
         }
     }
-    void Fire_Scatter(GameObject obj, Vector3 target)
+    void Fire_Scatter(Vector3 target)
     {
         for (int i = 0; i < projection_ea; i++)
         {
-            float projectionAngle = (i / projection_ea) * projection_Angle - projection_Angle * 0.5f;
+            GameObject obj = ObjectPoolManager.Instance.DequeueObject(Prefab_Projectile);
+
+            float projectionAngle = ((float)i / (float)projection_ea) * projection_Angle - projection_Angle * 0.5f;
 
             Quaternion angle = Quaternion.Euler(0, projectionAngle, 0);
 
-            Vector3 projectionVector = (target - this.transform.position) * speed_Projectile;
+            Vector3 projectionVector = (target - this.transform.position).normalized * speed_Projectile;
             projectionVector = angle * projectionVector;
 
             obj.GetComponent<Bullet>().Shoot(this.transform.position, projectionVector);
         }
     }
-    void Fire_Sniping(GameObject obj, Vector3 target)
+    void Fire_Sniping(Vector3 target)
     {
-        Vector3 projectionVector = (target - this.transform.position) * speed_Projectile;
+        GameObject obj = ObjectPoolManager.Instance.DequeueObject(Prefab_Projectile);
+
+        Vector3 projectionVector = (target - this.transform.position).normalized * speed_Projectile;
         obj.GetComponent<Bullet>().Shoot(this.transform.position, projectionVector);
     }
 }

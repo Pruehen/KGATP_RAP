02c7a70a188601e70d1ee0708173026c691896ec
권지호 <imperiaiiist@naver.com>
2024-07-15 13:55:41 +0900

    [Feat] 스턴 기능 구현

--- Assets/Scripts/LCH/Enemy.cs
+++ Assets/Scripts/LCH/Enemy.cs
@@ -9,16 +9,19 @@ namespace LCH
         [SerializeField] Transform target;
         [SerializeField] float enemyHp;
         [SerializeField] float atkDamage;        
-        [SerializeField] bool isTargeting;
-        [SerializeField] bool isStun;
+        [SerializeField] bool isTargeting;        
         [Range(0.1f, 10f)][SerializeField] float coolTime;
         [Header("장착 무기")]
         [SerializeField] EnemyWeapon weapon;
           
         private bool _isCanFire;
         private Animator _animator;
+        bool _isStun;
+        float _fireDelayTime;
+        float _stunTime;
 
         Action OnHit;
+        Action OnStun;
         Action OnDead;
 
         private void Start()
@@ -30,24 +33,30 @@ namespace LCH
             {
                 Nontarget_StartCoroutine_OnStart();
             }
-            else
-            {
-                StartCoroutine(ShootCoolTime());
-            }
             _animator = GetComponent<Animator>();
+            _fireDelayTime = coolTime;
         }
         private void Update()
         {
+            if(_isStun)
+            {
+                OnStun_OnUpdate();
+            }
+
             if (isTargeting)
             {
                 FindPlayer_OnUpdate();
-            }
+                ShootCoolTime_OnTargetingMode_OnUpdate();
+            }                        
         }
 
         void FindPlayer_OnUpdate()
         {
-            Vector3 targetPosition = new Vector3(target.position.x, transform.position.y, target.position.z);
-            transform.LookAt(targetPosition);
+            if (_isStun == false)
+            {
+                Vector3 targetPosition = new Vector3(target.position.x, transform.position.y, target.position.z);
+                transform.LookAt(targetPosition);
+            }
         }
         void Nontarget_StartCoroutine_OnStart()
         {
@@ -61,17 +70,24 @@ namespace LCH
         {
             while (true)
             {
-               transform.Rotate(0, 40, 0);
-                
-               weapon.CommandFire(this.gameObject.transform.forward + this.transform.position);
-               yield return new WaitForSeconds(coolTime);
-            }  
+                if (_isStun == false)
+                {
+                    transform.Rotate(0, 40, 0);
+
+                    weapon.CommandFire(this.gameObject.transform.forward + this.transform.position);
+                }
+                yield return new WaitForSeconds(coolTime);
+            }
         }
         
-        void Stun_OnUpdate()
-        {
 
-        }
+        public void Stun(float duration)
+        {
+            _isStun = true;
+            _stunTime = duration;
+            OnStun?.Invoke();
+        }        
+
         /// <summary>
         /// 피격 메서드
         /// </summary>
@@ -98,14 +114,26 @@ namespace LCH
             Destroy(this);
         }
 
-        IEnumerator ShootCoolTime()
+        void ShootCoolTime_OnTargetingMode_OnUpdate()
         {
-            while (true)
+            if (_isStun == false)
             {
+                _fireDelayTime -= Time.deltaTime;
+            }
+            if(_fireDelayTime <= 0)
+            {
+                _fireDelayTime = coolTime;
                 weapon.CommandFire(target);
-                yield return new WaitForSeconds(coolTime);
+            }            
+        }        
+
+        void OnStun_OnUpdate()
+        {            
+            _stunTime -= Time.deltaTime;
+            if(_stunTime <= 0)
+            {
+                _isStun = false;
             }
         }
-        
     }
 }

39397b0a32ef0d567f60c75a0af2bffbe1e316c2
K-Byeonggon <govleen79@gmail.com>
2024-07-15 09:33:26 +0900

    [feat] 기존 코드에 피격 무적 구현

--- Assets/Scripts/KBG/CommonBulletCopy.cs
+++ Assets/Scripts/KBG/CommonBulletCopy.cs
@@ -4,12 +4,15 @@ public class CommonBulletCopy : Bullet
 {
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.TryGetComponent(out PlayerHealth health))
+        if (collision.gameObject.TryGetComponent(out PlayerCopy2 player))
         {
+            /*
             DamageMessage dmgMessage = new DamageMessage();
             dmgMessage.damage = 1;
 
             health.ApplyDamage(dmgMessage);
+            */
+            player.Hit(1);
         }
         ProjectileDestroy(collision.contacts[0].point);
     }
